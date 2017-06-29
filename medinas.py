aabf410dca10098c718b6d72d05763aeaf5734f9
Naresh Annangar <hi@tulhan.in>
2017-06-29 16:54:32 +0530

    Implement class to handle domain names

--- medinas.py
+++ medinas.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-
+import struct
 from typing import List
 
 
@@ -38,6 +38,51 @@ class Resolver(object):
 
 class Name(object):
     """Stores a string and coverts it into DNS label wire format"""
+    domain_name = ''
 
     def __init__(self, domain_name: str):
-        pass
+        self.domain_name = domain_name
+
+    def __bytes__(self):
+        if not self.domain_name.endswith('.'):
+            self.domain_name = '{}.'.format(self.domain_name)
+
+        labels = self.domain_name.split('.')
+
+        wire = b''
+        for label in labels:
+            _ = label.encode()
+            stub = struct.pack('>B', len(_)) + _
+            wire += stub
+
+        return wire
+
+    def __str__(self):
+        return self.domain_name
+
+    def __eq__(self, other):
+        return self.__bytes__() == bytes(other)
+
+    @classmethod
+    def from_wire(cls, wire: bytes):
+        """Converts DNS labels wire format into a Name class"""
+        labels = []
+
+        while wire[0] != 0:
+            _len = int(wire[0])
+            if _len > 63:
+                raise DNSDecodeError()
+
+            label = wire[1:_len + 1]
+            wire = wire[_len + 1:]
+
+            labels.append(label.decode(encoding='utf-8'))
+
+        domain_name = '.'.join(labels)
+
+        return cls(domain_name)
+
+
+class DNSDecodeError(ValueError):
+    """Raised when errors are encountered during the decoding of the DNS wire format"""
+    pass
--- tests.py
+++ tests.py
@@ -2,7 +2,10 @@
 import unittest
 from unittest import TestCase
 
+from medinas import Name
 
+
+@unittest.skip('Not Implemented')
 class TestResolver(unittest.TestCase):
     def test_a(self):
         self.fail()
@@ -25,4 +28,26 @@ class TestResolver(unittest.TestCase):
 
 class TestName(TestCase):
     def test_name(self):
-        self.fail()
+        _ = Name('www.google.com')
+        self.assertEqual(_.__bytes__(), b'\x03\x77\x77\x77\x06\x67\x6f\x6f\x67\x6c\x65\x03\x63\x6f\x6d\x00')
+
+        _ = Name('gen.lib.rus.ec')
+        self.assertEqual(_.__bytes__(), b'\x03\x67\x65\x6e\x03\x6c\x69\x62\x03\x72\x75\x73\x02\x65\x63\x00')
+
+        _ = Name('www.microsoft.com.')
+        self.assertEqual(_.__bytes__(), b'\x03\x77\x77\x77\x09\x6d\x69\x63\x72\x6f\x73\x6f\x66\x74\x03\x63\x6f\x6d\x00')
+
+        _ = Name.from_wire(b'\x03\x77\x77\x77\x06\x67\x6f\x6f\x67\x6c\x65\x03\x63\x6f\x6d\x00')
+        self.assertEqual(str(_), 'www.google.com')
+
+        _ = Name.from_wire(b'\x03\x67\x65\x6e\x03\x6c\x69\x62\x03\x72\x75\x73\x02\x65\x63\x00')
+        self.assertEqual(str(_), 'gen.lib.rus.ec')
+
+        _ = Name.from_wire(b'\x03\x77\x77\x77\x09\x6d\x69\x63\x72\x6f\x73\x6f\x66\x74\x03\x63\x6f\x6d\x00')
+        self.assertEqual(str(_), 'www.microsoft.com')
+
+        self.assertEqual(Name('www.google.com'), Name('www.google.com.'))
+
+
+if __name__ == '__main__':
+    unittest.main()
