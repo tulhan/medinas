ac1b28b75e88e88e0091a334c4d461e25d91f4a4
Naresh Annangar <hi@tulhan.in>
2017-06-29 13:48:32 +0530

    Create directory structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+from distutils.core import setup
+
+setup(
+    name='medinas',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Naresh Annangar',
+    author_email='hi@tulhan.in',
+    description='A bare minimum DNS library'
+)
